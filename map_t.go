674bd6c2dccf7288c36e7f2d8a4ccf0124bdd06f
Administrator <654332905@qq.com>
2017-08-28 01:21:58 +0800

    learn to map

--- map_t.go
+++ map_t.go
@@ -5,24 +5,29 @@ import (
 )
 
 func main() {
+	// shiyanlou = {}
 	shiyanlou := make(map[string]string) // 与 map[string]string 相同
+	// shiyanlou["golang"] = "docker"
 	shiyanlou["golang"] = "docker"
 	shiyanlou["python"] = "flask web framework"
 	shiyanlou["linux"] = "sys administrator"
 	fmt.Print("Traverse all keys: ")
+	// for key in shiyanlou.keys()
 	for key := range shiyanlou { // 遍历了映射的所有键
+		// print('{}'.format(key))
 		fmt.Printf("% s ", key)
 	}
 	fmt.Println()
-
-	delete(shiyanlou, "linux")                  // 从映射中删除键"linux"及其值
-	shiyanlou["golang"] = "beego web framework" // 更新键“golang"的值
-
-	v, found := shiyanlou["linux"]
-	fmt.Printf("Found key \"linux\" Yes or False: %t, value of key \"linux\": \"%s\"", found, v)
+	// shiyanlou.delete('linux')
+	delete(shiyanlou, "linux")
+	shiyanlou["golang"] = "beego web framework"
+	// value, key 真他妈的反人类，把值放前面，没有就是空
+	v, key := shiyanlou["linux"]
+	fmt.Printf("Found key \"linux\" Yes or False: %t, value of key \"linux\": \"%s\"", key, v)
 	fmt.Println()
 
 	fmt.Println("Traverse all keys/values after changed:")
+	// for k,v in shiyanlou.items()
 	for k, v := range shiyanlou { //遍历了映射的所有键/值对
 		fmt.Printf("\"%s\": \"%s\"\n", k, v)
 	}
--- slice_array.go
+++ slice_array.go
@@ -3,16 +3,16 @@ package main
 import (
 	"fmt"
 )
-
+// 数组是按值传递的（即是传递的副本），而切片是引用类型，传递切片的成本非常小，而且是不定长的。而且数组是定长的，而切片可以调整长度。
 func main() {
+	// int 型数组
 	a := [...]int{1, 2, 3, 4, 5, 6, 7}
 	fmt.Printf("len and cap of array %v is: %d and %d\n", a, len(a), cap(a))
-	fmt.Printf("item in array: %v is:", a)
-	for _, value := range a {
-		fmt.Printf("% d", value)
+	fmt.Printf("item in array: %v is:\n", a)
+	// index, value
+	for i, value := range a {
+		fmt.Printf(" index: %d, value: %d\n", i, value)
 	}
-
-	fmt.Println()
 
 	s1 := a[3:6]
 	fmt.Printf("len and cap of slice: %v is: %d and %d\n", s1, len(s1), cap(s1))
@@ -24,14 +24,16 @@ func main() {
 	fmt.Println()
 
 	s1[0] = 456
-	fmt.Printf("item in array changed after changing slice: %v is:", s1)
+	// 切片是引用，所以会改变原来的值
+	fmt.Printf("item 'a' in array changed after changing slice: %v is:", s1)
 	for _, value := range a {
 		fmt.Printf("% d", value)
 	}
 
 	fmt.Println()
-
+	// 直接make一个slice
 	s2 := make([]int, 10, 20)
+	// index 4 change to  5
 	s2[4] = 5
 	fmt.Printf("len and cap of slice: %v is: %d and %d\n", s2, len(s2), cap(s2))
 	fmt.Printf("item in slice %v is:", s2)
