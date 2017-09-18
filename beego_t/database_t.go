9e10694f16159f7d0daaa9b3af6d2a876acb4745
Administrator <654332905@qq.com>
2017-09-19 00:42:54 +0800

    2017-09-19

--- beego_t/database_t.go
+++ beego_t/database_t.go
@@ -1,33 +1,58 @@
 package main
+
 import (
 	"github.com/astaxie/beego/orm"
 	_ "github.com/lib/pq"
 	"fmt"
-	"time"
 )
 
-type barrecord struct {
-	id int
-	user_id int
-	bar_id int
-	date time.Location
-	state int
+type User struct {
+	Id      int
+	Name    string
+	Profile *Profile `orm:"rel(one)"`      // OneToOne relation
+	Post    []*Post  `orm:"reverse(many)"` // 设置一对多的反向关系
+}
+
+type Profile struct {
+	Id   int
+	Age  int16
+	User *User `orm:"reverse(one)"` // 设置一对一反向关系(可选)
+}
+
+type Post struct {
+	Id    int
+	Title string
+	User  *User  `orm:"rel(fk)"` //设置一对多关系
+	Tags  []*Tag `orm:"rel(m2m)"`
+}
+
+type Tag struct {
+	Id    int
+	Name  string
+	Posts []*Post `orm:"reverse(many)"`
 }
 
 func init() {
+	// 需要在init中注册定义的model
+	orm.RegisterModel(new(User), new(Post), new(Profile), new(Tag))
 	// PostgreSQL 配置
 	orm.RegisterDriver("postgres", orm.DRPostgres) // 注册驱动
-	orm.RegisterDataBase("default", "postgres", "user=postgres password=postgres dbname=chat host=127.0.0.1 port=5432 sslmode=disable")
-
+	orm.RegisterDataBase("default", "postgres", "user=postgres password=postgres dbname=test host=127.0.0.1 port=5432 sslmode=disable")
 	// 自动建表
 	orm.RunSyncdb("default", false, true)
 }
 
 func main() {
-	orm.Debug = true
-	barr := new(barrecord)
-	barr.id=1
-	o := orm.NewOrm()  //注册新的orm
-	o.Read(barr)
-	fmt.Println(barr)
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+
+	profile := new(Profile)
+	profile.Age = 30
+
+	user := new(User)
+	user.Profile = profile
+	user.Name = "slene"
+
+	fmt.Println(o.Insert(profile))
+	fmt.Println(o.Insert(user))
 }
