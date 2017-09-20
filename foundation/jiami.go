bac5a244602925f5d7c4920956718c1ba371e237
Administrator <654332905@qq.com>
2017-09-21 00:30:10 +0800

    2017-09-21

--- foundation/jiami.go
+++ foundation/jiami.go
@@ -6,8 +6,11 @@ import (
 	"fmt"
 )
 
+
 func main()  {
 	h := sha256.New()
+	// 专家方案
+	//dk := scrypt.Key([]byte("some password"), []byte(salt), 16384, 8, 1, 32)
 	io.WriteString(h, "His money is twice tainted: 'taint yours and 'taint mine.")
 	fmt.Printf("% x", h.Sum(nil))
 }
@@ -24,3 +27,4 @@ func main()  {
 //h := md5.New()
 //io.WriteString(h, "需要加密的密码")
 //fmt.Printf("%x", h.Sum(nil))
+
