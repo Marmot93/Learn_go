21c2e42c0bc368077d178d226bd27dc684b57a72
marmot <654332905@qq.com>
2017-10-25 16:33:29 +0800

    beego orm create

--- beego_t/database_t.go
+++ beego_t/database_t.go
@@ -1,58 +1,82 @@
 package main
 
 import (
-	"github.com/astaxie/beego/orm"
-	_ "github.com/lib/pq"
 	"fmt"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq" //数据库驱动
 )
 
 type User struct {
-	Id      int
-	Name    string
-	Profile *Profile `orm:"rel(one)"`      // OneToOne relation
-	Post    []*Post  `orm:"reverse(many)"` // 设置一对多的反向关系
-}
-
-type Profile struct {
 	Id   int
 	Age  int16
-	User *User `orm:"reverse(one)"` // 设置一对一反向关系(可选)
-}
-
-type Post struct {
-	Id    int
-	Title string
-	User  *User  `orm:"rel(fk)"` //设置一对多关系
-	Tags  []*Tag `orm:"rel(m2m)"`
-}
-
-type Tag struct {
-	Id    int
-	Name  string
-	Posts []*Post `orm:"reverse(many)"`
+	Name string
 }
 
 func init() {
-	// 需要在init中注册定义的model
-	orm.RegisterModel(new(User), new(Post), new(Profile), new(Tag))
-	// PostgreSQL 配置
-	orm.RegisterDriver("postgres", orm.DRPostgres) // 注册驱动
-	orm.RegisterDataBase("default", "postgres", "user=postgres password=postgres dbname=test host=127.0.0.1 port=5432 sslmode=disable")
-	// 自动建表
-	orm.RunSyncdb("default", false, true)
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	orm.RegisterDataBase("default", "postgres", "host=127.0.0.1 port=5432 user=postgres password=postgres dbname=blog sslmode=disable")
+
+	orm.RegisterModel(new(User))
 }
 
-func main() {
+func insert() {
+
+	orm.Debug = true;
 	o := orm.NewOrm()
 	o.Using("default") // 默认使用 default，你可以指定为其他数据库
 
-	profile := new(Profile)
-	profile.Age = 30
+	user := User{Name: "marmot", Age: 24, Id:2}
 
-	user := new(User)
-	user.Profile = profile
-	user.Name = "slene"
+	fmt.Println(o.Insert(&user))
+}
 
-	fmt.Println(o.Insert(profile))
-	fmt.Println(o.Insert(user))
+func delete() {
+
+	orm.Debug = true;
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+
+	user := User{Id: 2}
+
+	fmt.Println(o.Delete(&user))
+}
+
+func update() {
+
+	orm.Debug = true;
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+
+	user := User{Id: 8, Name: "koome_new"}
+
+	fmt.Println(o.Update(&user))
+}
+
+func query() {
+
+	var j, i int64
+
+	orm.Debug = true;
+	o := orm.NewOrm()
+	o.Using("default") // 默认使用 default，你可以指定为其他数据库
+
+	var users [] User
+	num, err := o.Raw("SELECT age, id, name FROM public.user").QueryRows(&users)
+	fmt.Println(users)
+
+	if err == nil {
+		fmt.Println("user nums: ", num)
+	}
+
+	i = num
+	for j = 0; j < i; j++ {
+		fmt.Printf("Element[%d] : name:%s, age: %d\n", j, users[j].Name, users[j].Age)
+	}
+}
+
+func main() {
+	//insert()
+	//delete()
+	query()
 }
--- beego_t/hello.go
+++ beego_t/hello.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/astaxie/beego"
+	_ "github.com/lib/pq"
+	"time"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "blog"
 )
 
 type MainController struct {
 	beego.Controller
 }
 
-func (this *MainController) Get() {
-	this.Ctx.WriteString("hello world")
+func (self *MainController) Get() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+
+	var id, age int
+	var name string
+	var create_time time.Time
+	rows, err := db.Query(`SELECT id, age, create_time, "name" FROM student;`)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &age, &create_time, &name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		//fmt.Println(id, nickname, create_time.Format("2006-01-02 15:04:05"))
+		fmt.Println(id, age, name, create_time.Format("2006-01-02 15:04:05"))
+	}
+	self.Ctx.WriteString(name)
 }
 
 func main() {
