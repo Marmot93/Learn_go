b38dcf7e6851079d042a1586273631fe98eb946c
marmot <654332905@qq.com>
2017-10-27 18:02:46 +0800

    Some subject

--- ceshi.go
+++ ceshi.go
@@ -1,54 +1,24 @@
 package main
 
-import (
-	"database/sql"
-	"fmt"
-	_ "github.com/lib/pq"
-	"time"
-)
-
-
-
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "postgres"
-	dbname   = "blog"
-)
+import "fmt"
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-
-	var id,age int
-	var name  string
-	var create_time  time.Time
-	rows, err := db.Query(`SELECT id, age, create_time, "name" FROM student;`)
-	if err != nil{
-		panic(err)
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&id, &age, &create_time, &name)
-		if err != nil {
-			fmt.Println(err)
-		}
-		//fmt.Println(id, nickname, create_time.Format("2006-01-02 15:04:05"))
-		fmt.Println(id, age, name,create_time.Format("2006-01-02 15:04:05"))
-	}
-
+	var a int = 1
+	var b *int = &a
+	var c **int = &b
+	var x int = *b
+	fmt.Println("a = ", a)
+	fmt.Println("&a = ", &a)
+	fmt.Println("*&a = ", *&a)
+	fmt.Println("b = ", b)
+	fmt.Println("&b = ", &b)
+	fmt.Println("*&b = ", *&b)
+	fmt.Println("*b = ", *b)
+	fmt.Println("c = ", c)
+	fmt.Println("*c = ", *c)
+	fmt.Println("&c = ", &c)
+	fmt.Println("*&c = ", *&c)
+	fmt.Println("**c = ", **c)
+	fmt.Println("***&*&*&*&c = ", ***&*&*&*&*&c)
+	fmt.Println("x = ", x)
 }
