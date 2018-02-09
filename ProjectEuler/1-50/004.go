89b29f7c936038f41fd7658b629f843381de3b3e
marmot <654332905@qq.com>
2018-02-09 13:55:45 +0800

    欧拉计划 004

--- ProjectEuler/1-50/004.go
+++ ProjectEuler/1-50/004.go
@@ -9,23 +9,24 @@ import (
 //最大的由两个两位数乘积构成的回文数是 9009 = 91 * 99。
 //找出最大的有由个三位数乘积构成的回文数。
 
-func reverseString(s string) string {
+func is_palindrome(i int) bool {
+	s := strconv.Itoa(i)
 	runes := []rune(s)
 
 	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
 		runes[from], runes[to] = runes[to], runes[from]
 	}
 
-	return string(runes)
+	return string(runes) == s
 }
 
 func main() {
 	max := 0
 	for i := 999; i > 100; i-- {
 		for j := 999; j > 100; j-- {
-			s := strconv.Itoa(i * j)
-			if s == reverseString(s) && i*j > max {
-				max = i * j
+			n := i*j
+			if is_palindrome(n) && n > max {
+				max = n
 			}
 		}
 	}
