5e75a5087175a1c4b50ed5446de520b3391ee411
mouwenfeng <mouwenfeng@ouchteam.com>
2018-03-23 09:39:03 +0800

    1

--- web_t/database_sql.go
+++ web_t/database_sql.go
@@ -4,7 +4,6 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
-	"time"
 )
 
 const (
@@ -12,7 +11,7 @@ const (
 	port     = 5432
 	user     = "postgres"
 	password = "postgres"
-	dbname   = "blog"
+	dbname   = "param_record"
 )
 
 func main() {
@@ -30,22 +29,21 @@ func main() {
 
 	fmt.Println("Successfully connected!")
 
-	var id, age int
-	var name string
-	var create_time time.Time
-	rows, err := db.Query(`SELECT id, age, create_time, "name" FROM student;`)
+	var id int
+	var daily_temperature string
+	rows, err := db.Query(`SELECT id, daily_temperature  FROM bms_service_forecast;`)
 	if err != nil {
 		panic(err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		err := rows.Scan(&id, &age, &create_time, &name)
+		err := rows.Scan(&id, &daily_temperature)
 		if err != nil {
 			fmt.Println(err)
 		}
 		//fmt.Println(id, nickname, create_time.Format("2006-01-02 15:04:05"))
-		fmt.Println(id, age, name, create_time.Format("2006-01-02 15:04:05"))
+		fmt.Println(id, daily_temperature)
 	}
 
 }
