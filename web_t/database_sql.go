9109d194d6aa946ce53984ac50a9f6a6ad2d7e25
mouwenfeng <mouwenfeng@ouchteam.com>
2018-03-23 15:52:59 +0800

    sql预编译查询

--- web_t/database_sql.go
+++ web_t/database_sql.go
@@ -30,20 +30,28 @@ func main() {
 	fmt.Println("Successfully connected!")
 
 	var id int
-	var daily_temperature string
-	rows, err := db.Query(`SELECT id, daily_temperature  FROM bms_service_forecast;`)
+	var dailyTemperature string
+	sqlString := `SELECT id, daily_temperature  FROM bms_service_forecast WHERE id=$1;`
+	//rows, err := db.Query(`SELECT id, daily_temperature  FROM bms_service_forecast;`)
+
+	stmt, err := db.Prepare(sqlString)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer stmt.Close()
+
 	if err != nil {
 		panic(err)
 	}
-	defer rows.Close()
+	rows, err := stmt.Query(3)
 
 	for rows.Next() {
-		err := rows.Scan(&id, &daily_temperature)
+		err := rows.Scan(&id, &dailyTemperature)
 		if err != nil {
 			fmt.Println(err)
 		}
 		//fmt.Println(id, nickname, create_time.Format("2006-01-02 15:04:05"))
-		fmt.Println(id, daily_temperature)
+		fmt.Println(id, dailyTemperature)
 	}
 
 }
