45915f226d38b70578198d470efd1aebc4a364f3
mouwenfeng <mouwenfeng@ouchteam.com>
2018-05-31 17:01:11 +0800

    2018-05-31 tcp and udp

--- ceshi.go
+++ ceshi.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-// 等待所有 goroutine 执行完毕
-// 使用传址方式为 WaitGroup 变量传参
-// 使用 channel 关闭 goroutine
-
 func main() {
-	s := fmt.Sprintf("%v", math.Pi)
-	fmt.Println(s)
+	  {
+		fmt.Println(1111)
+	}
 }
